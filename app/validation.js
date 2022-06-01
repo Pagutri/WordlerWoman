56d7b717bd2976a8c6097f60426702c85c60a7f6
AndreaCuriel <acurielipn@hotmail.com>
2022-06-01 13:40:18 -0500

    #6 Agregando método estatico para la validación de coincidencias entre la palabra del usuario y la de la API

--- app/validation.js
+++ app/validation.js
@@ -1,24 +1,43 @@
-
 class Validation {
-    static wordValidation(API, User){
-        if (User == null) {
-            return "Requiero una palabra válida";
-        }
-        else if (!/^[a-z]+$/.test(User)){
-            return "La palabra debe contener solo letras";
-        }
-        else if (API.length !== User.length){
-            return "La palabra requiere 5 letras";
-        }
-        else if (API != User){
-            return "Intenta de nuevo";
-        }
-        else {
-            return "Acertaste";
-        }
+  static wordValidation(API, User) {
+    if (User == null) {
+      return "Requiero una palabra válida";
+    } else if (!/^[a-z]+$/.test(User)) {
+      return "La palabra debe contener solo letras";
+    } else if (API.length !== User.length) {
+      return "La palabra requiere 5 letras";
+    } else if (API != User) {
+      return this.wordCoincidences(API, User);
+    } else {
+      return "Acertaste";
     }
+  }
+
+  //Método de obtención de coincidencias entre la palabra de la API con la del Usuario.
+  static wordCoincidences(API, User) {
+    const arrUser = User.split("");
+    const arrAPI = API.split("");
+    var positionAPI = 0;
+    var positionUser = 0;
+    arrAPI.forEach((letterAPI) => {
+      positionAPI = positionAPI + 1;
+      positionUser = 0;
+      arrUser.forEach((letterUser) => {
+        positionUser = positionUser + 1;
+        if (letterAPI == letterUser) {
+          if (positionAPI === positionUser) {
+            return console.log(
+              `Letra:  ${letterAPI} en Posicion Correcta: ${positionAPI}`
+            );
+          } else {
+            return console.log("La palabra tiene la letra " + letterAPI);
+          }
+        } else {
+          return console.log("No contiene las letra");
+        }
+      });
+    });
+  }
 }
 
 module.exports = Validation;
-
-
