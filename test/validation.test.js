30ad490953a04501610379acd7c235246467362f
AndreaCuriel <acurielipn@hotmail.com>
2022-06-09 15:06:40 -0500

    Prueba Unitaria del método wordCoincidences

--- test/validation.test.js
+++ test/validation.test.js
@@ -25,21 +25,20 @@ describe("Validation test", () => {
     const answer = Validation.wordValidation(API, User);
     expect(answer).toBe("Acertaste");
   });
- 
+
   test("5. Validation method wordCoincidences: Arreglo de 5 caracteres", () => {
     const User = "lilos";
     const API = "hilos";
     const answer = Validation.wordCoincidences(API, User);
-    if(User.length<5||API.length<5)
-      {
-        expect(answer).toBe("Longitud incorrecta");
-      } 
+    if (User.length < 5 || API.length < 5) {
+      expect(answer).toBe("Longitud incorrecta");
+    }
   });
-  /*
+
   test("6. Validation method wordCoincidences: Posición correcta", () => {
-    const User = "lilos";
+    const User = "hielo";
     const API = "hilos";
     const answer = Validation.wordCoincidences(API, User);
-    expect(answer).toBe(`Letra:  ${letterAPI} en Posicion Correcta: ${positionAPI}`);
-  });*/
-}); 
+    expect(answer.toString()).toBe("2,2,0,1,1");
+  });
+});
